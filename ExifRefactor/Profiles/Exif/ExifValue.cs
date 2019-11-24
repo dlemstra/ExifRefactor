a8b477d501b9b387733eb86e2d5d58684e815aca
Dirk Lemstra <dirk@lemstra.org>
2019-11-24 22:46:00 +0100

    Changed IEquatable<> of ExifValue.

--- ExifRefactor/Profiles/Exif/ExifValue.cs
+++ ExifRefactor/Profiles/Exif/ExifValue.cs
@@ -2,7 +2,7 @@
 
 namespace ExifRefactor
 {
-    public abstract class ExifValue : IEquatable<ExifValue>
+    public abstract class ExifValue : IEquatable<ushort>
     {
         internal ExifValue(ExifTag tag)
         {
@@ -19,44 +19,19 @@ namespace ExifRefactor
 
         public abstract bool TrySetValue(object value);
 
-        public static bool operator ==(ExifValue left, ExifValue right) => Equals(left, right);
+        public static bool operator ==(ExifValue left, ushort right) => Equals(left, right);
 
-        public static bool operator !=(ExifValue left, ExifValue right) => !Equals(left, right);
-
-        public static bool operator ==(ExifValue left, ushort right)
-        {
-            if (left == null)
-                return false;
-
-            return left.Tag == right;
-        }
-
-        public static bool operator !=(ExifValue left, ushort right)
-        {
-            if (left == null)
-                return true;
-
-            return left.Tag != right;
-        }
+        public static bool operator !=(ExifValue left, ushort right) => !Equals(left, right);
 
         public override bool Equals(object obj)
         {
-            if (obj is ExifValue value)
+            if (obj is ushort value)
                 return Equals(value);
 
             return false;
         }
 
-        public bool Equals(ExifValue other)
-        {
-            if (other is null)
-                return false;
-
-            if (ReferenceEquals(this, other))
-                return true;
-
-            return Tag == other.Tag;
-        }
+        public bool Equals(ushort other) => Tag == other;
 
         public override int GetHashCode() => Tag.GetHashCode();
     }
