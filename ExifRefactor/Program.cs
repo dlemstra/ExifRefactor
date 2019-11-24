2296c989351b7771d07faf977d9a033c48e7c209
Dirk Lemstra <dirk@lemstra.org>
2019-11-24 21:52:22 +0100

    Improved the example in program.

--- ExifRefactor/Program.cs
+++ ExifRefactor/Program.cs
@@ -8,13 +8,15 @@ namespace ExifRefactor
         {
             var profile = new ExifProfile();
             profile.SetValue(ExifTag.PixelXDimension, 10);
+            profile.SetValue(ExifTag.CodingMethods, 2U);
 
             var data = profile.ToByteArray();
             profile = new ExifProfile(data);
 
             Console.WriteLine(profile.GetValue(ExifTag.PixelXDimension));
+            Console.WriteLine(profile.GetValue(ExifTag.CodingMethods));
 
-            profile.SetValue(ExifTag.CodingMethods, 2U);
+            profile.SetValue(ExifTag.CodingMethods, 4U);
             foreach (var value in profile.Values)
             {
                 if (value == ExifTag.PixelXDimension)
